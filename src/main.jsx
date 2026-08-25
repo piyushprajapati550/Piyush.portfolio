5673619c74b09d3d4167945896f13a945048122d
Piyush Kumar Prajapati <pk@Piyushs-MacBook-Air.local>
2026-08-25 10:34:46 +0530

    Add Google Analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const GA_MEASUREMENT_ID = 'G-9X3J0PEW0G'
+
+// Load Google Analytics
+const script = document.createElement('script')
+script.async = true
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
+document.head.appendChild(script)
+
+window.dataLayer = window.dataLayer || []
+function gtag() {
+  window.dataLayer.push(arguments)
+}
+
+gtag('js', new Date())
+gtag('config', GA_MEASUREMENT_ID)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
